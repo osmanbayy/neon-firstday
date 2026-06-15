f5e2e097a86a637b0841e7f3d37d82256cd8ff41
Osman Bay <bayosman342@gmail.com>
2026-06-15 16:33:36 +0300

    feat: update API URL in .env, add axios dependency, and create axios instance for API requests

---
+++ src/lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "";
+
+export const api = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
